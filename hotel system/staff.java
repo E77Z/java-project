0d90c0faa47c1752a3577ce331c1fcd2dc03339f
E77Z <ezzeldin0690x6@gmail.com>
2024-11-19 00:26:32 +0200

    Create staff.java

---
+++ hotel system/staff.java
@@ -0,0 +1,11 @@
+
+package full_hotel_sys;
+public class staff {
+      String staffID;
+    String password;
+    staff(String staffID, String password){
+        this.staffID = staffID;
+        this.password = password;
+    
+}
+}
